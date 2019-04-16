320a2b450aec5c6f2e98198e7268d67b3b867d70
sigefried <sigefriedhyy@gmail.com>
2019-04-16 22:16:09 +0900

    update template

--- template.cc
+++ template.cc
@@ -3,6 +3,7 @@
 #include<list>
 #include<deque>
 #include<queue>
+#include<deque>
 #include<stack>
 #include<map>
 #include<set>
@@ -14,14 +15,32 @@
 #include<cctype>
 #include<cmath>
 #include<iostream>
+#include<limits>
+#include<utility>
+
+#define c11
+#ifdef c11
+#include<unordered_set>
+#include<unordered_map>
+#endif
 
 using namespace std;
 typedef long long ll;
+#define ull unsigned long long
+#define PI acos(-1.0)
+#define eps 1e-12
 #define cls(x) memset(x,0,sizeof(x))
+#define forn(i, n) for(int i = 0; i < (int) n; ++i)
+#define for1(i, n) for(int i = 1; i <= (int) n; ++i)
+#define fore(i, l, r) for(int i = (int) l; i <= (int) r; ++i)
+#define rforn(i, n) for(int i = (int) n - 1; i>= 0; ++i)
 
+int dx[4] = {1, 0 , -1, 0};
+int dy[4] = {0, 1 , 0, -1};
 
 const int INF = 0x3f3f3f3f;
 const int SZ = 100;
+
 
 int main() {
 	return 0;
